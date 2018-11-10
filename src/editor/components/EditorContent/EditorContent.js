438d4aab105f88c678143e4749dcf43e70fc794a
simonghales <simonghales@gmail.com>
2018-11-11 01:28:30 +1000

    adding styles tab

--- src/editor/components/EditorContent/EditorContent.js
+++ src/editor/components/EditorContent/EditorContent.js
@@ -1,25 +1,51 @@
 // @flow
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles';
-import EditorComponentTabs from './components/EditorComponentTabs/EditorComponentTabs';
+import EditorComponentTabs, {
+  editorComponentTabs,
+} from './components/EditorComponentTabs/EditorComponentTabs';
 import EditorComponentProps from './components/EditorComponentProps/EditorComponentProps';
 import Button, { buttonTypes } from '../../../components/Button/Button';
 import type { DataBlockModel } from '../../../data/blocks/models';
+import type { EditorComponentTabsOptions } from './components/EditorComponentTabs/EditorComponentTabs';
 
 type Props = {
   selectedBlock: DataBlockModel,
 };
 
-const EditorContent = ({ selectedBlock }: Props) => (
-  <div className={styles.containerClass}>
-    <EditorComponentTabs />
-    <div className={styles.mainClass}>
-      <div className={styles.addPropClass}>
-        <Button type={buttonTypes.slim}>Add Prop</Button>
+type State = {
+  selectedTab: EditorComponentTabsOptions,
+};
+
+class EditorContent extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      selectedTab: editorComponentTabs.Props,
+    };
+  }
+
+  handleSelectTab = (tab: EditorComponentTabsOptions) => {
+    this.setState({
+      selectedTab: tab,
+    });
+  };
+
+  render() {
+    const { selectedBlock } = this.props;
+    const { selectedTab } = this.state;
+    return (
+      <div className={styles.containerClass}>
+        <EditorComponentTabs selectedTab={selectedTab} selectTab={this.handleSelectTab} />
+        <div className={styles.mainClass}>
+          <div className={styles.addPropClass}>
+            <Button type={buttonTypes.slim}>Add Prop</Button>
+          </div>
+          <EditorComponentProps selectedBlock={selectedBlock} />
+        </div>
       </div>
-      <EditorComponentProps selectedBlock={selectedBlock} />
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default EditorContent;
--- src/editor/components/EditorContent/components/EditorComponentTabs/EditorComponentTabs.js
+++ src/editor/components/EditorContent/components/EditorComponentTabs/EditorComponentTabs.js
@@ -4,15 +4,37 @@ import { MdBrush, MdCode } from 'react-icons/md';
 import { cx } from 'emotion';
 import styles from './styles';
 
-const EditorComponentTabs = () => (
+export const editorComponentTabs = {
+  Props: 'Props',
+  Styles: 'Styles',
+};
+
+export type EditorComponentTabsOptions = $Keys<typeof editorComponentTabs>;
+
+type Props = {
+  selectedTab: EditorComponentTabsOptions,
+  selectTab: (tab: EditorComponentTabsOptions) => void,
+};
+
+const EditorComponentTabs = ({ selectedTab, selectTab }: Props) => (
   <nav className={styles.containerClass}>
-    <div className={cx(styles.tabClass, styles.activeTabClass)}>
+    <div
+      className={cx(styles.tabClass, {
+        [styles.activeTabClass]: selectedTab === editorComponentTabs.Props,
+      })}
+      onClick={() => selectTab(editorComponentTabs.Props)}
+    >
       <MdCode />
-      <span>Props</span>
+      <span>{editorComponentTabs.Props}</span>
     </div>
-    <div className={styles.tabClass}>
+    <div
+      className={cx(styles.tabClass, {
+        [styles.activeTabClass]: selectedTab === editorComponentTabs.Styles,
+      })}
+      onClick={() => selectTab(editorComponentTabs.Styles)}
+    >
       <MdBrush />
-      <span>Styles</span>
+      <span>{editorComponentTabs.Styles}</span>
     </div>
   </nav>
 );
