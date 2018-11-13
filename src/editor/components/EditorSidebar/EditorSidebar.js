ede85a07ebbe6ee2e367c7e9a9e06e84ed6d08bb
simonghales <simonghales@gmail.com>
2018-11-13 22:48:30 +1000

    remove beautiful dnd

--- src/editor/components/EditorSidebar/EditorSidebar.js
+++ src/editor/components/EditorSidebar/EditorSidebar.js
@@ -1,8 +1,6 @@
 // @flow
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { DragDropContext } from 'react-beautiful-dnd';
-import type { DropResult, ResponderProvided } from 'react-beautiful-dnd';
 import styles from './styles';
 import type { MappedDataBlocks } from '../../../data/blocks/models';
 import type { ReduxState } from '../../../state/redux/store';
@@ -23,26 +21,12 @@ type Props = {
 };
 
 class EditorSidebar extends Component<Props> {
-  onDragEnd = (result: DropResult, provided: ResponderProvided) => {
-    console.log('drag ended', result, provided);
-    const { updateBlocks } = this.props;
-    const targetKey = result.draggableId;
-    const destinationKey = result.destination ? result.destination.droppableId : '';
-    const destinationIndex = result.destination ? result.destination.index : -1;
-    const sourceKey = result.source ? result.source.droppableId : '';
-    if (targetKey && destinationKey && sourceKey) {
-      updateBlocks(targetKey, destinationKey, destinationIndex, sourceKey);
-    }
-  };
-
   render() {
     const { blocks, selectBlock, selectedBlock } = this.props;
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <div className={styles.containerClass}>
-          <BlocksList blocks={blocks} selectBlock={selectBlock} selectedBlock={selectedBlock} />
-        </div>
-      </DragDropContext>
+      <div className={styles.containerClass}>
+        <BlocksList blocks={blocks} selectBlock={selectBlock} selectedBlock={selectedBlock} />
+      </div>
     );
   }
 }
--- src/editor/components/EditorSidebar/components/BlockPreview/BlockPreview.js
+++ src/editor/components/EditorSidebar/components/BlockPreview/BlockPreview.js
@@ -1,22 +1,9 @@
 // @flow
 import React from 'react';
 import { cx } from 'emotion';
-import { Droppable } from 'react-beautiful-dnd';
-import { Draggable } from 'react-beautiful-dnd';
 import type { MappedDataBlocks } from '../../../../../data/blocks/models';
 import styles from '../../styles';
 import BlocksList from '../BlocksList/BlocksList';
-
-const DroppableWrapper = ({ children, blockKey }: { children: any, blockKey: string }) => (
-  <Droppable droppableId={blockKey}>
-    {(provided, snapshot) => (
-      <div ref={provided.innerRef} {...provided.droppableProps}>
-        {children}
-        {provided.placeholder}
-      </div>
-    )}
-  </Droppable>
-);
 
 type Props = {
   type: string,
@@ -54,34 +41,16 @@ const BlockPreview = ({
       <div className={styles.blockPreviewTypeClass}>{type}</div>
       <div className={styles.blockPreviewLabelClass}>{label}</div>
     </div>
-    <DroppableWrapper blockKey={blockKey}>
-      {blockChildren.length > 0 && (
-        <div className={styles.blockPreviewChildrenClass}>
-          <BlocksList
-            blocks={blockChildren}
-            selectBlock={selectBlock}
-            selectedBlock={selectedBlock}
-          />
-        </div>
-      )}
-    </DroppableWrapper>
+    {blockChildren.length > 0 && (
+      <div className={styles.blockPreviewChildrenClass}>
+        <BlocksList
+          blocks={blockChildren}
+          selectBlock={selectBlock}
+          selectedBlock={selectedBlock}
+        />
+      </div>
+    )}
   </div>
 );
 
-const DraggableWrapper = (props: Props) => {
-  const { blockKey, isParentModule } = props;
-  if (isParentModule) {
-    return <BlockPreview {...props} />;
-  }
-  return (
-    <Draggable draggableId={blockKey} index={0}>
-      {(provided, snapshot) => (
-        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-          <BlockPreview {...props} />
-        </div>
-      )}
-    </Draggable>
-  );
-};
-
-export default DraggableWrapper;
+export default BlockPreview;
